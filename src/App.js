09442b5b81d76aea9b86b542f2e710dc7011953f
Trevor K. Jemmott Jr <tafittechtt@gmail.com>
2023-06-24 00:08:09 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import { useEffect } from 'react';
+import './App.css';
 
 
 function App() {
+
+  useEffect(() =>{
+    const loadProvider = async () => {
+    // Metamask have access to windows.ethereum & to window.web3
+    // metamask injects a global API into website
+    // this API allows websites to request users, account,
+    //read data to Blockchain, send messages and Transactions
+    }
+
+    loadProvider()
+  }, [])
+
   return (
-    <div className="App">
-      
-       Tafit Lottery 
-      
-     
+    <>
+    <div className="tafit-lottery-wapper">
+      <div className="Tafit Lottery">
+        <div className="balance-view is-size-2">
+        Congratulation<strong> YOU WON</strong> !!!
+        </div>
+        <b>Pay to Play:</b><strong> 0.001</strong> ETH
+        <div>
+      <button className="btn mr-2">Pay Here</button>
+      </div>
+     </div>
     </div>
+    </>
   );
 }
 
