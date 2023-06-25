daca0035c4461a8abc595a122bdaa1c71ebe33fd
Trevor K. Jemmott Jr <tafittechtt@gmail.com>
2023-06-25 05:28:52 -0400

    real faucet commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Web3 from 'web3';
+import detectEthereumProvider from '@metamask/detect-provider';
+import { loadContract } from './utils/load-contract';
+
+
 
 function App() {
+  const [web3Api, setWebApi] = useState({
+    provider: null,
+    web3: null
+  })
+
+  const [balance, setbalance] = useState(null)
+  const [account, setAccount] = useState(null)
+
+  useEffect(() =>{
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider()
+      const contract = await loadContract("tafitLottery", provider)
+
+
+      debugger
+      if (provider) {
+        
+        setWebApi({
+        web3: new Web3(provider),
+        provider,
+        contract
+        })
+      } else {
+        console.error("PLease, install Metamask")
+      }
+    }
+
+    loadProvider()
+  }, [])
+
+  useEffect(() => {
+    const loadBalance = async () => {
+      const{ contract, web3} = web3Api
+      const balance = await web3.eth.getbalance(contract.address)
+      setbalance(balance)
+      
+    }
+
+    web3Api.contract && loadBalance()
+  }, [web3Api])
+
+  useEffect (() =>{
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts()
+      setAccount(accounts[0])
+    }
+  
+    web3Api.web3 && getAccount()
+  }, [web3Api.web3])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="tafit-lottery-wrapper">
+      <div className="Tafit Lottery">
+        <div className=" is-flex is-align-items-center">
+          <span>
+            <div className="is-flex is-align-items-top">{balance}</div>
+          <strong className=" mr-2">Account:</strong>
+          </span>
+        <h1>
+          {account ? <div>{account}</div> : 
+          <button className="button is-info"
+            onClick={() =>
+              web3Api.provider.request({method:"eth_requestAccounts"}
+              )}
+          >
+            Connect Wallet
+          </button>
+          }
+        </h1>
+
+        </div>
+        <div className="balance-view is-size-2">
+        Congratulation<strong> YOU WON</strong> !!!
+        </div>
+        <b>Pay to Play:</b><strong> 0.001</strong> ETH
+        <div>    
+      <button className="button is-primary  is-active">Pay Here</button>
+      </div>
+     </div>
     </div>
   );
 }
