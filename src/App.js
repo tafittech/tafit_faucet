8cb7c59340d31823daf991694a94042b79777833
Trevor K. Jemmott Jr <tafittechtt@gmail.com>
2023-07-02 06:16:07 -0400

    Tafit wallet commit

--- src/App.js
+++ src/App.js
@@ -2,77 +2,40 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Web3 from 'web3';
 import detectEthereumProvider from '@metamask/detect-provider';
-import { loadContract } from './utils/load-contract';
 
 
 function App() {
-  const [web3Api, setWebApi] = useState({
-    provider: null,
-    web3: null,
-    contract: null
-  })
+ 
+     
 
-  const [account, setAccount] = useState(null)
-
-  useEffect(() =>{
-    const loadProvider = async () => {
-      const provider = await detectEthereumProvider()
-      const contract = await loadContract("tafitLottery")
-
-      if (provider) {
-        setWebApi({
-        web3: new Web3(provider),
-        provider,
-        contract
-        })
-      } else {
-        console.error("PLease, install Metamask")
-      }
-    }
-
-    loadProvider()
-  }, [])
-
-  useEffect (() =>{
-    const getAccount = async () => {
-      const accounts = await web3Api.web3.eth.getAccounts()
-      setAccount(accounts[0])
-    }
   
-    web3Api.web3 && getAccount()
-  }, [web3Api.web3])
-
   return (
-    <>
-    <div className="tafit-lottery-wrapper">
-      <div className="Tafit Lottery">
-        <div className=" is-flex is-align-items-center">
-          <span>
-          <strong className=" mr-2">Account:</strong>
-          </span>
-        <h1>
-          {account ? <div>{account}</div> : 
-          <button className="button is-info"
-            onClick={() =>
-              web3Api.provider.request({method:"eth_requestAccounts"}
-              )}
-          >
-            Connect Wallet
-          </button>
-          }
-        </h1>
 
+    
+    <div className="tafit-lottery-wrapper">
+      <div class="container">
+          <div class="iphone">
+            <div class="header">
+              <div class="header-summary">
+          <div class="summary-text">
+            My Balance
+          </div>
+          <div class="summary-balance">
+            &#8377; 293.00
+          </div>
+          <div class="summary-text-2">
+            +&nbsp;280.00
+          </div>
         </div>
-        <div className="balance-view is-size-2">
-        Congratulation<strong> YOU WON</strong> !!!
+        <div class="user-profile">
+          <img src="https://pbs.twimg.com/profile_images/1089189690773712897/bCJr7IzK_400x400.jpg" class="user-photo">
+            </img>
         </div>
-        <b>Pay to Play:</b><strong> 0.001</strong> ETH
-        <div>    
-      <button className="button is-primary  is-active">Pay Here</button>
       </div>
-     </div>
+        
     </div>
-    </>
+  </div>
+  </div>  
   );
 }
 
--- src/utils/load-contract.js
+++
@@ -1,8 +0,0 @@
-import contract from "@truffle/contract"
-
-export const loadContract = async (name) => {
-    await fetch('/contracts/${}.json')
-    const Artifact = await res.json()
-
-    return contract(Artifact)
-}
